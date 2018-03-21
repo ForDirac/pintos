1f1fb8f7876015d3e1b59228fb8e7bca3c3b61e6
Nyoungwoo <leenw2@kaist.ac.kr>
2018-03-21 19:03:40 +0900

    Nothing

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -89,9 +89,10 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
-  int64_t start = timer_ticks ();
-
-  ASSERT (intr_get_level () == INTR_ON);
+  int64_t start = timer_ticks (); /*read the Current time */ 
+  ASSERT (intr_get_level () == INTR_ON); /*To make "Timer Interrupt" */
+  
+  /*Give up to another thread during the ticks */
   while (timer_elapsed (start) < ticks) 
     thread_yield ();
 }
